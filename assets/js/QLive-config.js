0f585839dd488190b61aba73ef2707276656d49b
qlaiqyc <qyc123abc>
2017-05-12 23:24:29 +0800

    add  employ  search /findByPO

--- assets/js/QLive-config.js
+++ assets/js/QLive-config.js
@@ -1,2 +1,25 @@
-/*! ntq QL 2017-05-05 23:05:28 */
-"use strict";PageInfo.register({type:"config",info:function(){var a=location.origin+"/"+location.pathname.split("/")[1];return{paths:{jquery:a+"/assets/js/plug/jquery-3.2.0.js",layui:a+"/assets/js/plug/layui/lay/dest/layui.all.js"}}}});
+"use strict";
+
+PageInfo.register({ "type": "config", "info": function info() {
+
+		/*
+  * 01 :设计路径时需要判断是否是 本地 测试 正式 
+  * 02 :cdn 优化
+  * 
+  * */
+
+		var baseURL = location.origin + "/" + location.pathname.split("/")[1];
+
+		// var baseURL = "http://192.168.2.178:8020/ntq";
+
+		var result = {
+
+			paths: {
+				"jquery": baseURL + "/assets/js/plug/jquery-3.2.0.js",
+				"layui": baseURL + "/assets/js/plug/layui/lay/dest/layui.all.js"
+			}
+		};
+
+		return result;
+	} });
+//# sourceMappingURL=QLive-config.js.map
